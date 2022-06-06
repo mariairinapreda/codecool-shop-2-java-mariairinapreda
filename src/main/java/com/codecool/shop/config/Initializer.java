8931c20a345e9d76a893905c333fe44ec4fda8b1
mariairinapreda <mariairina.predaeu@gmail.com>
2022-06-06 09:25:25 +0300

    Separation for database

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -1,16 +1,9 @@
 package com.codecool.shop.config;
 
-import com.codecool.shop.dao.CartDao;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.CartDaoImpl;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import com.codecool.shop.service.DaoImplementation;
 import com.codecool.shop.service.ShopService;
 
 import javax.servlet.ServletContextEvent;
@@ -23,12 +16,9 @@ public class Initializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        CartDao cartDao= CartDaoImpl.getInstance();
 
-        ShopService shopService=ShopService.getInstance(productDataStore, productCategoryDataStore, supplierDataStore, cartDao);
+        ShopService shopService=ShopService.getInstance();
+        shopService.setImpl(DaoImplementation.IN_MEMORY);
         //setting up a new supplier
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
         shopService.getSupplierDao().add(amazon);
@@ -41,22 +31,22 @@ public class Initializer implements ServletContextListener {
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        shopService.getProductCategoryDao().getAll().add(tablet);
         ProductCategory phone = new ProductCategory("Phone", "Hardware", "A cell phone computer, commonly shortened to phone, is portable usually cordless telephone for use in a cellular system.");
-        productCategoryDataStore.add(phone);
+        shopService.getProductCategoryDao().getAll().add(phone);
         ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "A laptop, laptop computer, or notebook computer is a small, portable personal computer (PC) with a screen and alphanumeric keyboard.");
-        productCategoryDataStore.add(laptop);
+        shopService.getProductCategoryDao().getAll().add(laptop);
 
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("IPhone 13", new BigDecimal("799.9"), "USD", "Superbright, Supercolorful, Supersharp.", phone, apple));
-        productDataStore.add(new Product("tilifon", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", phone, apple));
-        productDataStore.add(new Product("tilifon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", phone, huawei));
-        productDataStore.add(new Product("tableta Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, apple));
-        productDataStore.add(new Product("laptop  a Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", laptop, amazon));
+        shopService.getProductDao().getAll().add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        shopService.getProductDao().getAll().add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+        shopService.getProductDao().getAll().add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        shopService.getProductDao().getAll().add(new Product("IPhone 13", new BigDecimal("799.9"), "USD", "Superbright, Supercolorful, Supersharp.", phone, apple));
+        shopService.getProductDao().getAll().add(new Product("tilifon", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", phone, apple));
+        shopService.getProductDao().getAll().add(new Product("tilifon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", phone, huawei));
+        shopService.getProductDao().getAll().add(new Product("tableta Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, apple));
+        shopService.getProductDao().getAll().add(new Product("laptop  a Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", laptop, amazon));
     }
 
 }
