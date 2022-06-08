33dfe00abef38bfd6a53a6d81458523f9a0aae1d
mariairinapreda <mariairina.predaeu@gmail.com>
2022-06-08 13:34:02 +0300

    DeleteProductApi

---
+++ src/main/java/com/codecool/shop/controller/api/DeleteProductApi.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.controller.api;
+
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.service.DaoImplementation;
+import com.codecool.shop.service.ShopService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"api/delete-product"}, loadOnStartup = 2)
+public class DeleteProductApi extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ShopService shopService=ShopService.getInstance();
+        shopService.setImpl(DaoImplementation.IN_MEMORY);
+        StringBuffer stringB=new StringBuffer();
+        String line;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null) {
+                stringB.append(line.charAt(7));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        int id= Integer.parseInt(stringB.toString());
+        shopService.getCartDao().getAll().removeIf(lineItem -> lineItem.getProduct().getId() == id);
+        response.setContentType("application/json");
+        String responseData = "{\"result\":\"success\"}";
+        PrintWriter out = response.getWriter();
+        out.println(responseData);
+    }
+
+}
