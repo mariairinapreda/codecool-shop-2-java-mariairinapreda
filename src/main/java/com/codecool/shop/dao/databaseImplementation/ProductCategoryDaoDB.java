9576d47af2979e6422673b7b56f5c6be8136cb74
mariairinapreda <mariairina.predaeu@gmail.com>
2022-10-03 14:08:23 +0300

    implemented product category dao for database

--- src/main/java/com/codecool/shop/dao/databaseImplementation/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/databaseImplementation/ProductCategoryDaoDB.java
@@ -11,11 +11,11 @@ import java.util.Objects;
 
 public class ProductCategoryDaoDB implements ProductCategoryDao {
     private final DataSource dataSource;
-    private static ProductCategoryDaoDB instance=null;
+    private static ProductCategoryDaoDB instance = null;
 
-    public static ProductCategoryDaoDB getInstance(DataSource dataSource){
-        if(instance==null)instance=new ProductCategoryDaoDB(dataSource);
-        return  instance;
+    public static ProductCategoryDaoDB getInstance(DataSource dataSource) {
+        if (instance == null) instance = new ProductCategoryDaoDB(dataSource);
+        return instance;
     }
 
     public ProductCategoryDaoDB(DataSource dataSource) {
@@ -24,22 +24,23 @@ public class ProductCategoryDaoDB implements ProductCategoryDao {
 
     @Override
     public void add(ProductCategory category) {
-        List<ProductCategory> categories=getAll();
-        boolean condition=false;
+        List<ProductCategory> categories = getAll();
+        boolean condition = false;
         for (ProductCategory productCategory : categories) {
-            if(Objects.equals(productCategory.getName(), category.getName())){
-                condition=true;
+            if (Objects.equals(productCategory.getName(), category.getName())) {
+                condition = true;
             }
         }
-        try(Connection conn = dataSource.getConnection()) {
-            if(!condition){
+        try (Connection conn = dataSource.getConnection()) {
+            if (!condition) {
                 String sql = "INSERT INTO categories (name) VALUES (?)";
-            PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            st.setString(1,category.getName());
-            st.executeUpdate();
-            ResultSet rs = st.getGeneratedKeys();
-            rs.next(); // Read next returned value - in this case the first one. See ResultSet docs for more explaination
-            category.setId(rs.getInt(1));}
+                PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                st.setString(1, category.getName());
+                st.executeUpdate();
+                ResultSet rs = st.getGeneratedKeys();
+                rs.next(); // Read next returned value - in this case the first one. See ResultSet docs for more explaination
+                category.setId(rs.getInt(1));
+            }
 
         } catch (SQLException throwables) {
             throw new RuntimeException("Error while adding category.", throwables);
@@ -49,7 +50,7 @@ public class ProductCategoryDaoDB implements ProductCategoryDao {
 
     @Override
     public ProductCategory find(int id) {
-       return  getAll().stream().filter(productCategory -> productCategory.getId()==id).findFirst().get();
+        return getAll().stream().filter(productCategory -> productCategory.getId() == id).findFirst().get();
 
     }
 
@@ -65,12 +66,13 @@ public class ProductCategoryDaoDB implements ProductCategoryDao {
             ResultSet rs = conn.createStatement().executeQuery(sql);
             List<ProductCategory> result = new ArrayList<>();
             while (rs.next()) {
-                ProductCategory productCategory = new ProductCategory(rs.getString(2),"department", "description");
+                ProductCategory productCategory = new ProductCategory(rs.getString(2), "department", "description");
                 productCategory.setId(rs.getInt(1));
                 result.add(productCategory);
             }
             return result;
         } catch (SQLException e) {
             throw new RuntimeException("Error while reading all categories", e);
-        }    }
+        }
+    }
 }
