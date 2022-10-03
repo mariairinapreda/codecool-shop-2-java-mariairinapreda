6b04e32b653382c694cd0c0f6d7390050fe6d53d
mariairinapreda <mariairina.predaeu@gmail.com>
2022-10-03 14:11:52 +0300

    update for cart and PaymentController

--- src/main/java/com/codecool/shop/controller/AuthorizePaymentController.java
+++ src/main/java/com/codecool/shop/controller/AuthorizePaymentController.java
@@ -1,15 +1,15 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.utils.CheckoutInfo;
-import com.codecool.shop.model.utils.PaymentInfo;
 import com.codecool.shop.model.utils.crunchifyPayPalSDKTutorial;
 import com.codecool.shop.serialization.Serialization;
+import com.codecool.shop.service.ShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,43 +17,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet("/authorize-payment")
-public class AuthorizePaymentController  extends HttpServlet{
-        TemplateEngine templateEngine;
-        WebContext webContext;
-        HttpSession httpSession;
-        private static Logger logger = LoggerFactory.getLogger(PaymentController.class);
-        private Serialization<CheckoutInfo> serialization = new Serialization();
+public class AuthorizePaymentController extends HttpServlet {
+    TemplateEngine templateEngine;
+    WebContext webContext;
+    HttpSession httpSession;
+    private static Logger logger = LoggerFactory.getLogger(PaymentController.class);
+    private Serialization<CheckoutInfo> serialization = new Serialization();
 
-        private void setData(HttpServletRequest request, HttpServletResponse response){
-            templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-            webContext = new WebContext(request, response, request.getServletContext());
-            httpSession = request.getSession();
-        }
-
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            setData(req, resp);
-            templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-            webContext = new WebContext(req, resp, req.getServletContext());
-            templateEngine.process("product/checkout.html", webContext,  resp.getWriter());
-        }
-
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            String productName = req.getParameter("product");
-            String subtotal = req.getParameter("subtotal");
-            String shipping = req.getParameter("shipping");
-            String tax = req.getParameter("tax");
-            String total = req.getParameter("total");
-            if(total != null){
-                crunchifyPayPalSDKTutorial crunchifyObj = new crunchifyPayPalSDKTutorial();
-                crunchifyObj.crunchifyCapturePayPalAPI(new CheckoutInfo(productName,subtotal, shipping, tax, total));
-                logger.info(serialization.serialization(new CheckoutInfo(productName,subtotal, shipping, tax, total)));
-                resp.sendRedirect(req.getContextPath()+"/payment");
-            }else {
-                resp.sendRedirect(req.getContextPath() + "/authorize-payment");
-            }
-        }
-
+    private void setData(HttpServletRequest request, HttpServletResponse response) {
+        templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        webContext = new WebContext(request, response, request.getServletContext());
+        httpSession = request.getSession();
     }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setData(req, resp);
+        ShopService shopService = ShopService.getInstance();
+        List<LineItem> items = shopService.getAllProdFromCart();
+        int subTotal = 0;
+        for (LineItem item : items) {
+            int productPrice = item.getProduct().getDefaultPrice().intValue();
+            subTotal += item.getQuantity() * productPrice;
+        }
+        int shippingInt = 10;
+        int taxInt = 10;
+        int totalInt = subTotal + shippingInt + taxInt;
+        templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        webContext = new WebContext(req, resp, req.getServletContext());
+        webContext.setVariable("product", "products");
+        webContext.setVariable("subtotal", subTotal);
+        webContext.setVariable("shipping", shippingInt);
+        webContext.setVariable("tax", taxInt);
+        webContext.setVariable("total", totalInt);
+        templateEngine.process("product/checkout.html", webContext, resp.getWriter());
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+
+        String productName = req.getParameter("product");
+        String subtotal = req.getParameter("subtotal");
+        String shipping = req.getParameter("shipping");
+        String tax = req.getParameter("tax");
+        String total = req.getParameter("total");
+
+        if (total != null) {
+            crunchifyPayPalSDKTutorial crunchifyObj = new crunchifyPayPalSDKTutorial();
+            crunchifyObj.crunchifyCapturePayPalAPI(new CheckoutInfo(productName, subtotal, shipping, tax, total));
+            logger.info(serialization.serialization(new CheckoutInfo(productName, subtotal, shipping, tax, total)));
+            resp.sendRedirect(req.getContextPath() + "/payment");
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/authorize-payment");
+        }
+    }
+
+}
--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,27 +1,17 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.CartDao;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.CartDaoImpl;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.*;
 import com.codecool.shop.service.DaoImplementation;
 import com.codecool.shop.service.ShopService;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sound.sampled.Line;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -31,11 +21,10 @@ public class CartController extends HttpServlet {
     TemplateEngine templateEngine;
     WebContext webContext;
     ShopService shopService;
-    HttpSession session;
 
 
-    private void setData(HttpServletRequest request,HttpServletResponse response) throws SQLException {
-        shopService=ShopService.getInstance();
+    private void setData(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        shopService = ShopService.getInstance();
         shopService.setImpl(DaoImplementation.IN_DATABASE);
         templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
 
@@ -49,18 +38,18 @@ public class CartController extends HttpServlet {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        List<LineItem> productList=shopService.getAllProdFromCart();
-        int sum=0;
+        List<LineItem> productList = shopService.getAllProdFromCart();
+        shopService.getLineItemDaoDB().add(productList);
+        int sum = 0;
+        int numberOfProd = 0;
         for (LineItem product : productList) {
-            sum+= product.getProduct().getDefaultPrice().intValue() *product.getQuantity();
-        }
-        int numberOfProd=0;
-        for (LineItem lineItem : productList) {
-            numberOfProd+=lineItem.getQuantity();
+            sum += product.getProduct().getDefaultPrice().intValue() * product.getQuantity();
+            numberOfProd += product.getQuantity();
+
         }
         webContext.setVariable("numberOfProd", numberOfProd);
-        webContext.setVariable("totalPrice",sum);
+        webContext.setVariable("totalPrice", sum);
         webContext.setVariable("items", productList);
         templateEngine.process("product/cart.html", webContext, resp.getWriter());
     }
-    }
+}
