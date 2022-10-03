4809b5962ec62028bc11537819ea5df22bdf442c
mariairinapreda <mariairina.predaeu@gmail.com>
2022-10-03 14:06:00 +0300

    run code formatter

--- src/main/java/com/codecool/shop/dao/CartDao.java
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.Product;
 
@@ -7,7 +8,10 @@ import java.util.List;
 
 public interface CartDao {
     void add(LineItem lineItem);
+
     Product find(int id);
+
     void remove(int id);
+
     List<LineItem> getAll();
 }
--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -1,16 +1,21 @@
 package com.codecool.shop.dao;
 
-import com.codecool.shop.model.Product;
 import com.codecool.shop.model.User;
 
 public interface UserDao {
     void add(User user);
+
     User find(int id);
+
     void remove(int id);
+
     boolean isLoggedIn(String email);
 
     void setPaymentMethod(String paypal);
+
     void updateStatus(String status, int id);
+
+    User getLastUser();
 
     boolean verifyPassword(String pass);
 }
